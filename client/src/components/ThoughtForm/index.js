a02cffdb412514594c6c455fda7ac9bf3fbddc40
spencercreer <spencercreer@gmail.com>
2021-07-13 22:22:33 -0600

    added route to upload image to AWS S3 bucket

--- client/src/components/ThoughtForm/index.js
+++ client/src/components/ThoughtForm/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 
 const ThoughtForm = () => {
   const [formState, setFormState] = useState({
@@ -6,6 +6,7 @@ const ThoughtForm = () => {
     thought: "",
   });
   const [characterCount, setCharacterCount] = useState(0);
+  const fileInput = useRef(null);
 
   // update state based on form input changes
   const handleChange = (event) => {
@@ -60,6 +61,21 @@ const ThoughtForm = () => {
           className="form-input col-12 "
           onChange={handleChange}
         ></textarea>
+        <label className="form-input col-12  p-1">
+          Add an image to your thought:
+          <input
+            type="file"
+            ref={fileInput}
+            className="form-input p-2"
+          />
+          <button
+            className="btn"
+            onClick={handleImageUpload}
+            type="submit"
+          >
+            Upload
+          </button>
+        </label>
         <button className="btn col-12 " type="submit">
           Submit
         </button>
